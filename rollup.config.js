462f32482e1993c11f6294769929d41531845c68
Chihiro Kaimori <kaimori@uzuworks.net>
2020-07-05 23:47:15 +0900

    [add] basic files

---
+++ rollup.config.js
@@ -0,0 +1,93 @@
+import { terser } from "rollup-plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+
+const jsfiles = {
+  script: './src/js_src/main.js'
+}
+const cssfiles = {
+  style: './src/css_src/main.scss'
+}
+
+export default [{
+  input: jsfiles,
+  output: [
+    {
+      name: 'script',
+      dir: `${__dirname}/dist/assets`,
+      entryFileNames: 'js/[name].js',
+      sourcemap: true,
+      format: 'iife',
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelrc: false,
+      configFile: './babel.config.json',
+    })
+  ],
+}, {
+  input: jsfiles,
+  output: [
+    {
+      name: 'script',
+      dir: `${__dirname}/dist/assets`,
+      entryFileNames: 'js/[name].min.js',
+      sourcemap: false,
+      format: 'iife',
+      plugins: [terser()]
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelrc: false,
+      configFile: './babel.config.json',
+    })
+  ],
+}, {
+  input: cssfiles,
+  output: [
+    {
+      name: 'style',
+      dir: `${__dirname}/dist/assets`,
+      entryFileNames: 'css/[name].css',
+    }
+  ],
+  plugins: [
+    resolve(),
+    postcss({
+      use: 'sass',
+      extensions: ['.scss'],
+      minimize: false,
+      sourceMap: true,
+      config: { path: 'postcss.config.js' },
+      extract: 'css/style.css'
+    }),
+  ],
+}, {
+  input: cssfiles,
+  output: [
+    {
+      name: 'style',
+      dir: `${__dirname}/dist/assets`,
+      entryFileNames: 'css/[name].min.css',
+    }
+  ],
+  plugins: [
+    resolve(),
+    postcss({
+      use: 'sass',
+      extensions: ['.scss'],
+      minimize: true,
+      sourceMap: false,
+      config: { path: 'postcss.config.js' },
+      extract: 'css/style.min.css'
+    }),
+  ],
+}];
